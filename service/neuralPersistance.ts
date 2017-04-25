506148aea40a24556b1bcb970f3452cb942ee6a2
Vojtěch Baránek <vojtabaranek@gmail.com>
2017-04-26 00:03:21 +0200

    heroku direct json loading

--- service/neuralPersistance.ts
+++ service/neuralPersistance.ts
@@ -1,41 +1,46 @@
 import { Network, Architect } from "synaptic";
 import LSTM = Architect.LSTM;
 export default class NeuralPersist {
-  private path = require('path');
+    private path = require('path');
 
-  private fs = require('fs');
-  private address = this.path.join(__dirname, '../data/neuraldata15_15_15_15.json')
+    private fs = require('fs');
+    private address = this.path.join(__dirname, '../data/neuraldata15_15_15_15.json')
 
-  constructor() {
+    constructor() {
 
-  }
-
-  public persistFile(network) {
-    console.log('persisting file to ')
-    console.log(this.address)
-    this.fs.writeFileSync(this.address, JSON.stringify(network.toJSON()));
-  }
-
-  public readFile() {
-    console.log('reading file from ')
-    console.log(this.address)
-    try {
-      return JSON.parse(this.fs.readFileSync(this.address, 'utf8'));
-    } catch (e) {
-      return false;
     }
-  }
 
-  public static getNet(): Network {
-
-    const lastKnown = new NeuralPersist().readFile();
-
-    if (lastKnown) {
-      console.log('Network found.');
-      const network = Network.fromJSON(lastKnown);
-      return network;
-    } else {
-      throw new Error("No network found");
+    public persistFile(network) {
+        console.log('persisting file to ')
+        console.log(this.address)
+        this.fs.writeFileSync(this.address, JSON.stringify(network.toJSON()));
     }
-  }
+
+    public readFile() {
+        if (process.env.herokuEnv) {
+            console.log("heroku env, loading directly json")
+            const brain = require('../data/neuraldata15_15_15_15.json')
+            return brain;
+        }
+        console.log('reading file from ')
+        console.log(this.address)
+        try {
+            return JSON.parse(this.fs.readFileSync(this.address, 'utf8'));
+        } catch (e) {
+            return false;
+        }
+    }
+
+    public static getNet(): Network {
+
+        const lastKnown = new NeuralPersist().readFile();
+
+        if (lastKnown) {
+            console.log('Network found.');
+            const network = Network.fromJSON(lastKnown);
+            return network;
+        } else {
+            throw new Error("No network found");
+        }
+    }
 }
